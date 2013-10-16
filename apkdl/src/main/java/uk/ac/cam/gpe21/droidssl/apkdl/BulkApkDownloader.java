7d4f36c984274c97c0ad3639682664751e206a46
Graham Edgecombe <graham@grahamedgecombe.com>
2013-10-16 14:53:09 +0100

    Add Maven project structure.

---
+++ apkdl/src/main/java/uk/ac/cam/gpe21/droidssl/apkdl/BulkApkDownloader.java
@@ -0,0 +1,11 @@
+package uk.ac.cam.gpe21.droidssl.apkdl;
+
+/**
+ * TODO
+ * @author Graham Edgecombe
+ */
+public final class BulkApkDownloader {
+	public static void main(String[] args) {
+		System.out.println("hello, world!");
+	}
+}
