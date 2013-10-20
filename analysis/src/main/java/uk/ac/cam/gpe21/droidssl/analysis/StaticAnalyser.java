b8f9a10cc2cf871a7e1e2f5f72392027c37a6202
Graham Edgecombe <graham@grahamedgecombe.com>
2013-10-20 19:00:32 +0100

    Add initial Soot test: loading APKs and converting Dex to Jimple.

--- analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/StaticAnalyser.java
+++ analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/StaticAnalyser.java
@@ -1,6 +1,16 @@
 package uk.ac.cam.gpe21.droidssl.analysis;
 
+import soot.options.Options;
+
+import java.util.Arrays;
+
 public final class StaticAnalyser {
 	public static void main(String[] args) {
+		Options.v().set_src_prec(Options.src_prec_apk);
+		Options.v().set_output_format(Options.output_format_jimple);
+		Options.v().set_android_jars("/opt/android/platforms");
+		Options.v().set_process_dir(Arrays.asList("test-hv/build/apk/test-hv-release-unsigned.apk"));
+		Options.v().set_unfriendly_mode(true);
+		soot.Main.main(new String[0]);
 	}
 }
