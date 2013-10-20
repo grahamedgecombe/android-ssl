6820227fadc86dea7d45c3ef64e854b1a25f23eb
Graham Edgecombe <graham@grahamedgecombe.com>
2013-10-20 19:53:42 +0100

    Disable Soot output.

--- analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/StaticAnalyser.java
+++ analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/StaticAnalyser.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public final class StaticAnalyser {
 	public static void main(String[] args) {
 		Options.v().set_src_prec(Options.src_prec_apk);
-		Options.v().set_output_format(Options.output_format_jimple);
+		Options.v().set_output_format(Options.output_format_none);
 		Options.v().set_android_jars("/opt/android/platforms");
 		Options.v().set_process_dir(Arrays.asList("test-hv/build/apk/test-hv-release-unsigned.apk"));
 		Options.v().set_unfriendly_mode(true);
