91547142767b44a6dc74aac13d103598c49d2ba1
Graham Edgecombe <graham@grahamedgecombe.com>
2013-10-24 10:56:11 +0100

    Add X509TrustManager RefType constant.

--- analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/util/Types.java
+++ analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/util/Types.java
@@ -6,6 +6,7 @@ public final class Types {
 	public static RefType STRING = RefType.v("java.lang.String");
 	public static RefType SSL_SESSION = RefType.v("javax.net.ssl.SSLSession");
 	public static RefType HOSTNAME_VERIFIER = RefType.v("javax.net.ssl.HostnameVerifier");
+	public static RefType X509_TRUST_MANAGER = RefType.v("javax.net.ssl.X509TrustManager");
 
 	private Types() {
 		/* to prevent instantiation */
