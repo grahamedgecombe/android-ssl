7c4a038b75e3a5519fc11efe8c2967282e889fb7
Graham Edgecombe <graham@grahamedgecombe.com>
2013-10-31 21:38:39 +0000

    Make constants in Types final.

--- analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/util/Types.java
+++ analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/util/Types.java
@@ -4,21 +4,21 @@ import soot.ArrayType;
 import soot.RefType;
 
 public final class Types {
-	public static RefType STRING = RefType.v("java.lang.String");
-	public static ArrayType STRING_ARRAY = ArrayType.v(STRING, 1);
+	public static final RefType STRING = RefType.v("java.lang.String");
+	public static final ArrayType STRING_ARRAY = ArrayType.v(STRING, 1);
 
-	public static RefType ACTIVITY = RefType.v("android.app.Activity");
-	public static RefType VIEW = RefType.v("android.view.View");
+	public static final RefType ACTIVITY = RefType.v("android.app.Activity");
+	public static final RefType VIEW = RefType.v("android.view.View");
 
-	public static RefType SSL_SESSION = RefType.v("javax.net.ssl.SSLSession");
-	public static RefType SSL_EXCEPTION = RefType.v("javax.net.ssl.SSLException");
+	public static final RefType SSL_SESSION = RefType.v("javax.net.ssl.SSLSession");
+	public static final RefType SSL_EXCEPTION = RefType.v("javax.net.ssl.SSLException");
 
-	public static RefType HOSTNAME_VERIFIER = RefType.v("javax.net.ssl.HostnameVerifier");
-	public static RefType ABSTRACT_VERIFIER = RefType.v("org.apache.http.conn.ssl.AbstractVerifier");
+	public static final RefType HOSTNAME_VERIFIER = RefType.v("javax.net.ssl.HostnameVerifier");
+	public static final RefType ABSTRACT_VERIFIER = RefType.v("org.apache.http.conn.ssl.AbstractVerifier");
 
-	public static RefType X509_TRUST_MANAGER = RefType.v("javax.net.ssl.X509TrustManager");
+	public static final RefType X509_TRUST_MANAGER = RefType.v("javax.net.ssl.X509TrustManager");
 
-	public static RefType HTTPS_URL_CONNECTION = RefType.v("javax.net.ssl.HttpsURLConnection");
+	public static final RefType HTTPS_URL_CONNECTION = RefType.v("javax.net.ssl.HttpsURLConnection");
 
 	private Types() {
 		/* to prevent instantiation */
