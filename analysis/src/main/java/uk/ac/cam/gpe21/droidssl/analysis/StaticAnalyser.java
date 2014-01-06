ed69f8843c4b68cb255a89550dd7491ec4eccc14
Graham Edgecombe <graham@grahamedgecombe.com>
2014-01-06 18:33:04 +0000

    Swap stdout and stderr, add FAILED output.

--- analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/StaticAnalyser.java
+++ analysis/src/main/java/uk/ac/cam/gpe21/droidssl/analysis/StaticAnalyser.java
@@ -11,93 +11,107 @@ import java.util.Set;
 public final class StaticAnalyser {
 	public static void main(String[] args) {
 		if (args.length != 1) {
-			System.err.println("Usage:");
-			System.err.println("  java -cp ... " + StaticAnalyser.class.getName() + " apkfile");
+			System.out.println("Usage:");
+			System.out.println("  java -cp ... " + StaticAnalyser.class.getName() + " apkfile");
 			System.exit(1);
 		}
 
-		/*
-		 * Write Soot's debug messages to stderr.
-		 */
-		G.v().out = System.err;
+		try {
+			/*
+			 * Write Soot's debug messages to stdout.
+			 * (stderr would be more appropriate, but Soot has some code which uses
+			 * System.out.println() directly. Therefore we use stderr for the
+			 * actual output of interest, and stdout for everything else.)
+			 */
+			G.v().out = System.out;
 
-		/*
-		 * Set path to input APK.
-		 */
-		Options.v().set_src_prec(Options.src_prec_apk);
-		Options.v().set_process_dir(Collections.singletonList(args[0]));
+			/*
+			 * Set path to input APK.
+			 */
+			Options.v().set_src_prec(Options.src_prec_apk);
+			Options.v().set_process_dir(Collections.singletonList(args[0]));
 
-		/*
-		 * Prevent Soot from outputting *.jimple files.
-		 */
-		Options.v().set_output_format(Options.output_format_none);
+			/*
+			 * Prevent Soot from outputting *.jimple files.
+			 */
+			Options.v().set_output_format(Options.output_format_none);
 
-		/*
-		 * Set the path to the Android SDK. Whilst Soot contains code to try to
-		 * detect the target SDK version, some applications use methods only
-		 * present in the newer SDK, causing SPARK to crash. Therefore we force
-		 * the use of the latest version of the Android SDK (4.4).
-		 */
-		Options.v().set_force_android_jar("/opt/android/platforms/android-19/android.jar");
+			/*
+			 * Set the path to the Android SDK. Whilst Soot contains code to try to
+			 * detect the target SDK version, some applications use methods only
+			 * present in the newer SDK, causing SPARK to crash. Therefore we force
+			 * the use of the latest version of the Android SDK (4.4).
+			 */
+			Options.v().set_force_android_jar("/opt/android/platforms/android-19/android.jar");
 
-		/*
-		 * Allow phantom references as there are some Google-specific APIs not
-		 * included in the Android SDK directory as listed above.
-		 */
-		Options.v().set_allow_phantom_refs(true);
+			/*
+			 * Allow phantom references as there are some Google-specific APIs not
+			 * included in the Android SDK directory as listed above.
+			 */
+			Options.v().set_allow_phantom_refs(true);
 
-		/*
-		 * Enable the SPARK points-to analysis.
-		 */
-		Options.v().set_whole_program(true);
-		PhaseOptions.v().processPhaseOptions("cg.spark", "enabled:true");
+			/*
+			 * Enable the SPARK points-to analysis.
+			 */
+			Options.v().set_whole_program(true);
+			PhaseOptions.v().processPhaseOptions("cg.spark", "enabled:true");
 
-		Set<Vulnerability> vulnerabilities = new HashSet<>();
+			Set<Vulnerability> vulnerabilities = new HashSet<>();
 
-		/*
-		 * Add transforms to the Whole Jimple Pre-processing Pack.
-		 */
-		Pack wjpp = PackManager.v().getPack("wjpp");
-		wjpp.add(new Transform("wjpp.activity_entry_transformer", new EntryMethodTransformer()));
+			/*
+			 * Add transforms to the Whole Jimple Pre-processing Pack.
+			 */
+			Pack wjpp = PackManager.v().getPack("wjpp");
+			wjpp.add(new Transform("wjpp.activity_entry_transformer", new EntryMethodTransformer()));
 
-		/*
-		 * Add transforms to the Whole Jimple Transformation Pack.
-		 */
-		AnalysisTransformer transformer = new AnalysisTransformer(
-			vulnerabilities,
+			/*
+			 * Add transforms to the Whole Jimple Transformation Pack.
+			 */
+			AnalysisTransformer transformer = new AnalysisTransformer(
+				vulnerabilities,
 
-			/* find vulnerable HostnameVerifiers */
-			new KnownHostnameVerifierAnalyser(vulnerabilities),
-			new HostnameVerifierAnalyser(vulnerabilities),
-			new AbstractVerifierAnalyser(vulnerabilities),
+				/* find vulnerable HostnameVerifiers */
+				new KnownHostnameVerifierAnalyser(vulnerabilities),
+				new HostnameVerifierAnalyser(vulnerabilities),
+				new AbstractVerifierAnalyser(vulnerabilities),
 
-			/* find places where HostnameVerifiers are used */
-			new DefaultHostnameVerifierAnalyser(vulnerabilities),
-			new HttpsUrlConnectionAnalyser(vulnerabilities),
+				/* find places where HostnameVerifiers are used */
+				new DefaultHostnameVerifierAnalyser(vulnerabilities),
+				new HttpsUrlConnectionAnalyser(vulnerabilities),
 
-			/* find vulnerable X509TrustManagers */
-			new TrustManagerAnalyser(vulnerabilities),
+				/* find vulnerable X509TrustManagers */
+				new TrustManagerAnalyser(vulnerabilities),
 
-			/* find places where X509TrustManagers are used */
-			new SslContextAnalyser(vulnerabilities)
-		);
+				/* find places where X509TrustManagers are used */
+				new SslContextAnalyser(vulnerabilities)
+			);
 
-		Pack wjtp = PackManager.v().getPack("wjtp");
-		wjtp.add(new Transform("wjtp.analysis", transformer));
+			Pack wjtp = PackManager.v().getPack("wjtp");
+			wjtp.add(new Transform("wjtp.analysis", transformer));
 
-		/*
-		 * Perform the analysis.
-		 */
-		Scene.v().loadNecessaryClasses();
-		PackManager.v().runPacks();
+			/*
+			 * Perform the analysis.
+			 */
+			Scene.v().loadNecessaryClasses();
+			PackManager.v().runPacks();
 
-		/*
-		 * Print out the list of vulnerabilities.
-		 */
-		System.err.println(vulnerabilities.size() + " vulnerabilities:");
-		System.err.flush();
-		for (Vulnerability vulnerability : vulnerabilities) {
-			System.out.println(vulnerability.toString());
+			/*
+			 * Print out the list of vulnerabilities.
+			 */
+			System.out.println(vulnerabilities.size() + " vulnerabilities:");
+			System.out.flush();
+			for (Vulnerability vulnerability : vulnerabilities) {
+				System.err.println(vulnerability.toString());
+			}
+		} catch (Throwable t) {
+			/* use stdout for the same reason as above */
+			t.printStackTrace(System.out);
+
+			/*
+			 * Print out FAILED so we can tell the difference between a failure
+			 * or just a lack of vulnerabilities.
+			 */
+			System.err.println("\t\tFAILED");
 		}
 	}
 }
