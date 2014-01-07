aedf241e5c82245844fc5761248348f2026dc245
Graham Edgecombe <graham@grahamedgecombe.com>
2014-01-07 17:02:49 +0000

    Make PermissiveTrustManager extend X509ExtendedTrustManager.
    This prevents the problem where certificates with MD2 are marked as
    untrusted by the JVM.

--- mitm-common/src/main/java/uk/ac/cam/gpe21/droidssl/mitm/crypto/PermissiveTrustManager.java
+++ mitm-common/src/main/java/uk/ac/cam/gpe21/droidssl/mitm/crypto/PermissiveTrustManager.java
@@ -1,10 +1,12 @@
 package uk.ac.cam.gpe21.droidssl.mitm.crypto;
 
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.X509ExtendedTrustManager;
+import java.net.Socket;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
-public final class PermissiveTrustManager implements X509TrustManager {
+public final class PermissiveTrustManager extends X509ExtendedTrustManager {
 	@Override
 	public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 		/* empty */
@@ -18,5 +20,25 @@ public final class PermissiveTrustManager implements X509TrustManager {
 	@Override
 	public X509Certificate[] getAcceptedIssuers() {
 		return new X509Certificate[0];
+	}
+
+	@Override
+	public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket) throws CertificateException {
+		/* empty */
+	}
+
+	@Override
+	public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) throws CertificateException {
+		/* empty */
+	}
+
+	@Override
+	public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine) throws CertificateException {
+		/* empty */
+	}
+
+	@Override
+	public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine engine) throws CertificateException {
+		/* empty */
 	}
 }
